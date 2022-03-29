bde945f495f4b5e1c72e3552d76558f4fa3efbb4
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-03-29 10:08:52 +0300

    refactor: transfer static pages in public directory, remove action in ApplicationController, change application config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def about
-    render "layouts/about"
-  end
-
-  def author
-    render "layouts/author"
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ Bundler.require(*Rails.groups)
 
 module TestGuru
   class Application < Rails::Application
+    VERSION = '0.1.1'.freeze
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
     config.time_zone = 'Moscow'
 
-    config.i18n.load_path += Dir[Rails.root.join('my',  'locales', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :ru]
     config.i18n.default_locale = :ru
     # Settings in config/environments/* take precedence over those specified here.
