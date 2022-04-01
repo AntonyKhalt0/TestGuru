097261370adea0a869a08d0c7972f6f9891894f7
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-01 13:39:56 +0300

    refactor: migrations to change column null and create migrations to change column default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_083839) do
+ActiveRecord::Schema.define(version: 2022_04_01_103756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2022_04_01_083839) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
