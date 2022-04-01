553193152169a8f5f9f9eabd2361b77020aaa301
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-01 11:14:47 +0300

    build: create models and configuration db

--- db/migrate/20220331193631_create_answers.rb
+++ db/migrate/20220331193631_create_answers.rb
@@ -1,7 +1,6 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.integer :id
       t.string :body
       t.boolean :correct
       t.integer :questions_id
