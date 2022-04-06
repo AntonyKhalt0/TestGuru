799ee3138b46338f65beb120b2aed51e54d1a980
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-06 22:07:29 +0300

    build: created associations belongs_to, has_one and has_many for exists models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user, foreign_key: "author_id"
+  has_many :results
+  has_many :users, through: :results
+  has_many :questions
+
   class << self
     def test_names_in_descending_order(category_name)
       joins("INNER JOIN categories ON categories.id = tests.category_id")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :results
+  has_many :tests, through: :results
+  
   def test_history_with_level(level)
     Test.joins("INNER JOIN results ON results.test_id = tests.id", "INNER JOIN users ON results.user_id = users.id")
         .where(level: level)
