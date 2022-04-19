55c014755f045ab7d69ce4631c506e7ee5ab1578
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-19 19:38:38 +0300

    refactor: add shallow method in routes and changed methods in Questions Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,32 +4,38 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions = find_questions.all
+    @questions = find_questions
     render json: { questions: @questions }
   end
 
   def show
-    @question = find_questions.find(params[:id])
-    render json: { questions: @question }
+    @questionss = Question.find(params[:id])
+    render plain: @questionss.inspect
   end
 
   def create
-    question = Question.create(question_params)
-    render plain: question.inspect
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else 
+      render :new
+    end
   end
 
   def destroy
     find_questions.find(params[:id]).destroy
+    redirect_to :index
   end
 
   private
 
   def find_test
-    @test = Test.find(params[:test_id])
+    @test_id = params[:id]
+    @test = Test.find(@test_id)
   end
 
   def find_questions
-    find_test.questions
+    @test.questions
   end
   
   def question_params
