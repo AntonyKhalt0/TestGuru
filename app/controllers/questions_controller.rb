d5e5cb688f3cd99adf5564f15b9a0a4e96f34fdf
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-22 14:27:28 +0300

    build: created navigation link

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,12 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :new, :create, :update]
+  before_action :find_test, only: [:index, :new, :create]
   before_action :find_question, only: [:show, :edit, :update, :destroy]
+  before_action :find_test_by_question, only: [:show, :edit, :update, :destroy]
 
-  #rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = find_questions
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @question.update(question_params)
-      redirect_to test_questions_path(@test)
-    else 
-      render :show
-    end
   end
 
   def new
@@ -35,8 +22,15 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def update
+    if @question.update(question_params)
+      redirect_to test_questions_path(@test)
+    else 
+      render :show
+    end
+  end
+
   def destroy
-    @test = Test.find(@question.test_id)
     @question.destroy
     redirect_to test_questions_path(@test)
   end
@@ -45,6 +39,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_test_by_question
+    @test = Test.find(@question.test_id)
   end
 
   def find_question
