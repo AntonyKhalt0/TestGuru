0cbff55dc3a4644b9868bfd9870dfd141e9fccf7
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-04-22 21:16:45 +0300

    build: created methods in questions_helper and changed footer partial

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,15 @@
 module QuestionsHelper
+  GIT_URL = 'https://github.com'
+
+  def question_header(test)
+    test.title
+  end
+
+  def current_year
+    Time.zone.now.year
+  end
+
+  def github_url(author, repo)
+    "#{GIT_URL}/#{author}/#{repo}"
+  end
 end
