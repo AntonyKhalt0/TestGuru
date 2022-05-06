87e9198a535bc0c3181daa731d3924e6f4b51bc3
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-05-06 21:43:37 +0300

    build: created user authentication, navigation bar and email validation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelper
+  def show_flash(type)
+    content_tag(:p, flash[type], class: "flash #{type}") if flash[type]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,11 @@ class User < ApplicationRecord
   has_many :test_passages, dependent: :delete_all
   has_many :tests, through: :test_passages
 
+  EMAIL_PATTERN = /\A([a-zA-Z]|[0-9])+[.,]*([a-zA-Z]|[0-9])*@[a-zA-Z]+.+[a-z]{2,}\z/i
+
   has_secure_password
+
+  validates :email, uniqueness: true, format: { with: EMAIL_PATTERN }
 
   def test_history_by_complexity(complexity)
     tests.public_send(complexity) if %w[simple intermediate difficult].include? complexity
