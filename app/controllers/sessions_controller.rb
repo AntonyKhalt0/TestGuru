2bcaae54bb6e041da3c1f324b9d77d1a63ee1b11
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-05-07 15:40:10 +0300

    refactor: changed redirect user on original path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_back(fallback_location: root_path)
+      redirect_to session[:referer]
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
