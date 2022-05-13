ad92ff4f4167a11832aca9e883225e7758ed63cc
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-05-13 10:40:01 +0300

    build: restricting access to questions and answers resources

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,22 +1,15 @@
-class AnswersController < ApplicationController
-  before_action :authenticate_user!
+class Admin::AnswersController < Admin::BaseController
   before_action :set_question, only: %i[ new create ]
   before_action :set_answer, only: %i[ show edit update destroy ]
 
-  def show
-  end
-
   def new
-    @answer = Answer.new
-  end
-
-  def edit
+    @answer = @question.answers.build
   end
 
   def create
     @answer = @question.answers.build(answer_params)
     if @answer.save
-      redirect_to @answer
+      redirect_to admin_answer_path(@answer)
     else
       render :new
     end
@@ -24,7 +17,7 @@ class AnswersController < ApplicationController
 
   def update
     if @answer.update(answer_params)
-      redirect_to question_path(@question)
+      redirect_to admin_question_path(@question)
     else
       redirect_to :edit
     end
@@ -32,7 +25,7 @@ class AnswersController < ApplicationController
 
   def destroy
     @answer.destroy
-    redirect_to @answer.question
+    redirect_to admin_question_path(@answer.question)
   end
 
   private
--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,5 +1,4 @@
-class QuestionsController < ApplicationController
-  before_action :authenticate_user!
+class Admin::QuestionsController < Admin::BaseController
   before_action :set_test, only: [:new, :create]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   
@@ -13,7 +12,7 @@ class QuestionsController < ApplicationController
     @question = @test.questions.build(question_params)
 
     if @question.save
-      redirect_to test_path(@question.test)
+      redirect_to admin_test_path(@question.test)
     else 
       render :new
     end
@@ -21,7 +20,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to test_path(@question.test)
+      redirect_to admin_test_path(@question.test)
     else 
       render :show
     end
@@ -29,7 +28,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    redirect_to test_path(@question.test)
+    redirect_to admin_test_path(@question.test)
   end
 
   private
