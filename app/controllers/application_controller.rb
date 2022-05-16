1d64319399aa965d792c12d4cf3fae1bd5960f8f
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-05-16 13:46:57 +0300

    build: created localization files for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  def default_url_options
+    {} if I18n.locale == I18n.default_locale
+
+    { lang: I18n.locale }
+  end
+
+  private
+
   def after_sign_in_path_for(resource) 
     if resource.is_a?(Admin)
       admin_tests_path 
     else
       root_path
     end
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
