712c57eb359bb3f2d4ee71c813ce8581d298adec
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-05-26 14:44:53 +0300

    fix: changed password_match.js and password_match_class.js

--- app/assets/javascripts/password_match.js
+++ app/assets/javascripts/password_match.js
@@ -1,4 +1,7 @@
 document.addEventListener('turbolinks:load', function() {
-  const registrationPage = new PasswordMatch(document, document.querySelector('.check-pass'));
-  registrationPage.passwordsDefinition();
+  let page = document.querySelector('.check-pass');
+  let password = document.getElementById('user_password');
+  let password_confirmation = document.getElementById('user_password_confirmation');
+  const registrationPage = new PasswordMatch(page);
+  registrationPage.passwordsDefinition(password, password_confirmation);
 })  
--- app/assets/javascripts/password_match_class.js
+++ app/assets/javascripts/password_match_class.js
@@ -1,16 +1,13 @@
 class PasswordMatch {
 
-  constructor (doc, page) {
-    this.document = doc;
+  constructor (page) {
     this.page = page;
   }
 
-  passwordsDefinition () {
-    let password = this.document.getElementById('user_password');
-    let password_confirmation = this.document.getElementById('user_password_confirmation');
+  passwordsDefinition (password, password_confirmation) {
     let match = this.page.querySelector('.octicon-check');
     let noMatch = this.page.querySelector('.octicon-circle-slash');
-    this.document.addEventListener('input', function () {
+    this.page.addEventListener('input', function () {
       if (password.value != password_confirmation.value && password.value) {
         match.classList.add('hide');
         noMatch.classList.remove('hide');
