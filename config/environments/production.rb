9dbbc25da6a02b18e41d844fb77878e93b5988b6
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-06-04 18:03:35 +0300

    fix: changed bin/bundle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://best-test-guru.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://best-test-guru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     tls:                  true,
