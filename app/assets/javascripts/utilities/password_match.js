b9df06d5e67f3d478b2881608174548a80cea502
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-06-06 10:58:28 +0300

    fix: change progress bar error

--- app/assets/javascripts/utilities/password_match.js
+++ app/assets/javascripts/utilities/password_match.js
@@ -1,8 +1,8 @@
 document.addEventListener('turbolinks:load', function() {
   let page = document.querySelector('.password-match');
   console.log(page);
-  let password = document.getElementById('user_password');
-  let password_confirmation = document.getElementById('user_password_confirmation');
-  const registrationPage = new PasswordMatch(page);
-  registrationPage.passwordsDefinition(password, password_confirmation);
+  //let password = document.getElementById('user_password');
+  //let password_confirmation = document.getElementById('user_password_confirmation');
+  //const registrationPage = new PasswordMatch(page);
+  //registrationPage.passwordsDefinition(password, password_confirmation);
 })  
